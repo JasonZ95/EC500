89d60752c4a602524d084516a91fcb6be46f7722
JasonZ95 <43214200+JasonZ95@users.noreply.github.com>
2019-02-04 15:36:46 -0500

    Update patient_observer.py

--- patient_observer.py
+++ patient_observer.py
@@ -1,2 +1,7 @@
-def login(user_id, age, gender, password):
-
+def input(user_id, age, gender, password, permission):
+  if (permission == doctor):
+    #the user have the access to the patient cases in database, which contains the basic infomation and the fitness data
+  else:
+    #the user can only have the access to his own data
+#For the input function, users are devided into different user groups which has different permission to access the database.
+#Once connected, It will also connect the hardware device to the internet, which enable it to use store the data into the user's directory.
